96fa8608596028a685ca8b417f6ccbc8c8f64864
Joel Kuhn <joelakuhn@gmail.com>
2016-03-12 22:48:51 -0500

    Began transition to Ractive on connections page.

--- js/new-tab/new-tab.js
+++ js/new-tab/new-tab.js
@@ -1,5 +1,30 @@
 $(function() {
 
+function get_saved_connections() {
+  if (localStorage.saved_connections) {
+    return JSON.parse(localStorage.saved_connections);
+  }
+  else {
+    return [];
+  }
+}
+
+new_tab_ui = new Ractive({
+  data: {
+    connection: {
+      nickname: '',
+      server: '',
+      instance: '',
+      database: '',
+      user: '',
+      password: '',
+    },
+    connections: [],
+  },
+  el: '#tab-info',
+  template: '#tab-info-template'
+});
+
 function show_message(message) {
   $('#message').text(message);
 }
@@ -15,55 +40,30 @@ function new_tab(config) {
   });
 }
 
-function get_config() {
-  return {
-    server: $('#server').val(),
-    instance: $('#instance').val(),
-    database: $('#database').val(),
-    user: $('#user').val(),
-    password: $('#password').val()
-  };
-}
-
-function get_saved_connections() {
-  if (localStorage.saved_connections) {
-    return JSON.parse(localStorage.saved_connections);
-  }
-  else {
-    return {};
-  }
-}
-
-function get_nickname() {
-  var specified_nickname = $('#nickname').val();
-  if (specified_nickname) {
-    return specified_nickname;
-  }
-  else {
-    return $('#server').val();
-  }
-}
-
 $('#open').click(function() {
-  new_tab(get_config());
+  new_tab(new_tab_ui.get('connection'));
 });
 
 $('#save').click(function() {
-  var config = get_config();
+  var config = new_tab_ui.get('connection');
   var saved_connections = get_saved_connections();
-  var nickname = get_nickname();
-  saved_connections[nickname] = config;
+  var key = Math.floor(Math.random() * 1000000000).toString();
+  saved_connections[key] = config;
 
   localStorage.saved_connections = JSON.stringify(saved_connections);
 });
 
+$('#delete').click(function() {
+
+});
+
 function load_saved_connection(nickname, config) {
-  $('#nickname').val(nickname);
-  $('#server').val(config.server);
-  $('#instance').val(config.instance);
-  $('#database').val(config.database);
-  $('#user').val(config.user);
-  $('#password').val(config.password);
+  new_tab_ui.set('connection.nickname' , nickname);
+  new_tab_ui.set('connection.server'   , config.server);
+  new_tab_ui.set('connection.instance' , config.instance);
+  new_tab_ui.set('connection.database' , config.database);
+  new_tab_ui.set('connection.user'     , config.user);
+  new_tab_ui.set('connection.password' , config.password);
 }
 
 function saved_item_click() {
@@ -80,31 +80,30 @@ function saved_item_dblclick() {
   window.getSelection().removeAllRanges();
 }
 
-if (localStorage.saved_connections) {
-  var saved_connections = get_saved_connections();
-  for (var nickname in saved_connections) {
-    var list_item = $('<li>')
-    .text(nickname)
-    .click(saved_item_click)
-    .dblclick(saved_item_dblclick)
-    .appendTo('#sidebar-list');
-  }
-}
 
-$(function() {
-
-  $('#sidebar-list-filter').keyup(function() {
-    var filter = new RegExp($(this).val().toLowerCase(), 'i');
-    $('.sidebar-list li').each(function() {
-      if ($(this).text().match(filter)) {
-        $(this).removeClass('filtered');
-      }
-      else {
-        $(this).addClass('filtered');
-      }
-    });
+$('#sidebar-list-filter').keyup(function() {
+  var filter = new RegExp($(this).val().toLowerCase(), 'i');
+  $('.sidebar-list li').each(function() {
+    if ($(this).text().match(filter)) {
+      $(this).removeClass('filtered');
+    }
+    else {
+      $(this).addClass('filtered');
+    }
   });
+});
 
+var connections_ui = new Ractive({
+  data: {
+    connections: get_saved_connections()
+  },
+  el: '#sidebar-list',
+  template: '#sidebar-list-template'
+})
+.on({
+  select_connection: function(e) {
+    new_tab_ui.set('connection', e.context);
+  }
 })
 
 });
